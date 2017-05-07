3ea5a2ab88c860ea146f8074ac56958e9a4943c8
Lasse Støjier Pedersen <quer@quers.net>
2017-05-07 23:44:46 +0200

    get full set
    * look up steam emblem, and get collection
    * get steam inv, and group card by appid

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 const util = require('util');
+var fs = require('fs');
 var CardCollection = require('./lib/getCardCollection');
 var cardCollection = new CardCollection();
 var UsersCard = require('./lib/getUsersCard');
 var inv = new UsersCard();
-inv.getInvCollection('gamle', '753', '6', function (data) {
+var compleat = [];
+var notCompleat = [];
+/**
+ * [description]
+ * @param  {sting} steam api name eller steam64
+ * @param  {int} 1: steam api name 2: steam64
+ */
+inv.getInvCollection('quer_the_gamer', 1, function (data) {
 	//console.log(data);
 	//console.log(util.inspect(data, false, null));
-	console.log(data[730])
-	for (var k in data) {
-		console.log("loop");
-		console.log(k);
-		console.log(data[k]);
-		cardCollection.checkApp(data[k], k, function () {
-			console.log("done!");
-		});
-		break;
-	}
+	fs.writeFile('gamle.json', JSON.stringify(data), function(err2) {
+	    if(err2) {
+	        throw err2;
+	    }
+	    console.log("The file was saved!");
+	}); 
+	var keys = Object.keys(data);
+	customforEach(data, keys, 0, function () {
+		console.log("done");
+		console.log(compleat);
+		console.log(countEnd());
+		console.log(notCompleat);
+	})
 });
 
+function customforEach(data, keys, indexKey, callback) {
+
+	if (keys.length > indexKey && data[keys[indexKey]]) {
+		cardCollection.checkApp(data[keys[indexKey]], keys[indexKey], function (set) {
+			
+			if(set != null){
+				compleat.push(set);
+				console.log("null - no");
+			}else{
+				notCompleat.push(keys[indexKey]);
+				console.log("null");
+			}
+			customforEach(data, keys, ++indexKey, callback)
+		});
+	} else {
+		callback();
+	}
+}
+
+function countEnd() {
+	var end = {
+		setsTypes: compleat.length,
+		sets: 0
+	};
+	for (var i = 0; i < compleat.length; i++) {
+		end.sets += compleat[i].length;
+	}
+	return end;
+}
